2a583029e7dbd573b424f66720b64f65173896c6
ChiharuHub <152666667+ChiharuHub@users.noreply.github.com>
2023-12-02 10:25:24 +0900

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 force = new Vector3(10, 0, 10);
+        Rigidbody rbody = this.GetComponent<Rigidbody>();
+        rbody.AddForce(force, ForceMode.VelocityChange);
+    }
+}
